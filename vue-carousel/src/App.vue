609c1f9464bd75f897e0ac0db12e83fe2fd9f705
»Laika☆Dog«(Лайка) <junshinbee@gmail.com>
2021-06-01 19:02:49 +0700

    add draft sliding

--- vue-carousel/src/App.vue
+++ vue-carousel/src/App.vue
@@ -7,6 +7,7 @@
 <script>
 // import label-collection.json
 import LabelStorage from './asset/resource/label-storage.json';
+import DummyHomeData  from './dummy_data/dummy-home-data.json';
 // import components
 import TheHomePage from './components/page/the-home-page.vue';
 
@@ -17,11 +18,13 @@ export default {
   data() {
     return {
       labelStorage: LabelStorage,
+      dummyHomeData: DummyHomeData
     }
   },
   provide() {
     return {
-      labelStorage: this.labelStorage
+      labelStorage: this.labelStorage,
+      dummyHomeData: this.dummyHomeData
     }
   }
 };
--- vue-carousel/src/components/page/the-home-page.vue
+++ vue-carousel/src/components/page/the-home-page.vue
@@ -1,14 +1,17 @@
 <template>
   <user-interface-navigation-bar></user-interface-navigation-bar>
+  <user-interface-sliding-section></user-interface-sliding-section>
 </template>
 
 <script>
 // import components
 import UserInterfaceNavigationBar from '../user-interface/ui-navigation-bar.vue';
+import UserInterfaceSlidingSection from '../user-interface/ui-sliding-section.vue';
 
 export default {
   components: {
     UserInterfaceNavigationBar,
+    UserInterfaceSlidingSection
   },
 };
 </script>
